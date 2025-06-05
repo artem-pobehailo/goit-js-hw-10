49dcc75061ae343fe82cf1f5349ec68bf6224be2
artem-pobehailo <partemishe@gmail.com>
2025-06-05 11:25:50 +0000

    Deploying to gh-pages from @ artem-pobehailo/goit-js-hw-10@84089865c7fa00e0bf1e4abb9d91ae2ca497a00a 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log(11111);
+//# sourceMappingURL=1-timer.js.map
---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log(22222222);
+//# sourceMappingURL=2-snackbar.js.map
