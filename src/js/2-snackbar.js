589c6c30682f8a752cf5b9267214a679455bce3d
Artem <partemishe@gmail.com>
2025-06-06 13:46:40 +0300

    hw10 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,34 @@
-console.log(22222222);
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { delay, state } = event.target.elements;
+  const delayValue = +delay.value;
+  const stateValue = state.value;
+  setTimeout(() => {
+    return new Promise((resolve, reject) => {
+      if (stateValue === 'fulfilled') {
+        resolve('fulfilled');
+      } else {
+        reject('rejected');
+      }
+    })
+      .then(delay => {
+        iziToast.success({
+          message: `✅ Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          message: `❌ Rejected promise in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+  }, delayValue);
+}
